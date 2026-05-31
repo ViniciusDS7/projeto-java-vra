53d673d0c828c327e3499cf26563e8b4801af775
ViniciusDS7 <160297718+ViniciusDS7@users.noreply.github.com>
2026-05-31 20:15:23 -0300

    Update Usuario.java

--- model/Usuario.java
+++ model/Usuario.java
@@ -1,15 +1,20 @@
+// Classe responsável por representar um usuário do sistema
 public class Usuario {
 
     private String nome;
     private String email;
     private String cargo;
+    private String perfil;
 
-    public Usuario(String nome, String email, String cargo) {
+    // Construtor da classe
+    public Usuario(String nome, String email, String cargo, String perfil) {
         this.nome = nome;
         this.email = email;
         this.cargo = cargo;
+        this.perfil = perfil;
     }
 
+    // Métodos de acesso aos atributos
     public String getNome() {
         return nome;
     }
@@ -20,5 +25,9 @@ public class Usuario {
 
     public String getCargo() {
         return cargo;
+    }
+
+    public String getPerfil() {
+        return perfil;
     }
 }
