b3fd76bdfb4cdd8ded09d634295964d9faddf8da
ViniciusDS7 <160297718+ViniciusDS7@users.noreply.github.com>
2026-05-31 20:14:59 -0300

    Update Projeto.java

--- model/Projeto.java
+++ model/Projeto.java
@@ -1,13 +1,16 @@
+// Classe responsável por representar um projeto do sistema
 public class Projeto {
 
     private String nomeProjeto;
     private String status;
 
+    // Construtor da classe
     public Projeto(String nomeProjeto, String status) {
         this.nomeProjeto = nomeProjeto;
         this.status = status;
     }
 
+    // Exibe as informações do projeto
     public void exibirProjeto() {
 
         System.out.println("Projeto: " + nomeProjeto);
