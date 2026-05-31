48a3b3bcdbc3dee6dc2ebe517b9d12e26ac8183d
ViniciusDS7 <160297718+ViniciusDS7@users.noreply.github.com>
2026-05-31 20:15:41 -0300

    Update ProjetoController.java

--- controller/ProjetoController.java
+++ controller/ProjetoController.java
@@ -1,7 +1,7 @@
+// Classe responsável pelo controle das operações do sistema
 public class ProjetoController {
 
     public void iniciarProjeto() {
-
         System.out.println("Projeto Freaky iniciado com sucesso!");
     }
 }
